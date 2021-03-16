b6e4bba4c383da185111a96124ed948065ec13b4
Doug Murray <murray@twinleaf.com>
2021-03-16 16:33:41 -0400

    Added Javascript to change image if clicked.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	let mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/godzilla-icon.jpg') {
+		myImage.setAttribute('src', 'images/firefox.png');
+	} else {
+		myImage.setAttribute('src', 'images/godzilla-icon.jpg');
+	}
+}
